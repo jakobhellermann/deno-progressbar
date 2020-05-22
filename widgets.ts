631dfb99019612ffcabf79201524a239dca557df
Craig Morris <craig.michael.morris@gmail.com>
2020-05-22 10:54:38 +0100

    Fix percentage formula

--- widgets.ts
+++ widgets.ts
@@ -1,6 +1,6 @@
 import { WidgetFn } from "./progressbar.ts";
 
 export const percentageWidget: WidgetFn = (i: number, total: number) =>
-  `${(i / total).toFixed(2)}%`;
+  `${(i / total * 100).toFixed(2)}%`;
 export const amountWidget: WidgetFn = (i: number, total: number) =>
   `${i}/${total}`;
